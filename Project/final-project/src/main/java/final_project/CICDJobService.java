7ba8b6e9d714226658705850a8b153c4f64f7ea4
tomer idan <105118970+tomerIdan@users.noreply.github.com>
2024-08-12 22:17:01 +0300

    Update CICDJobService.java

--- Project/final-project/src/main/java/final_project/CICDJobService.java
+++ Project/final-project/src/main/java/final_project/CICDJobService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,5 +40,9 @@ public class CICDJobService {
 
     public void deleteJob(Long id) {
         ciCdJobRepository.deleteById(id);
+    }
+
+    protected void onCreate() {
+        Date createdAt = new Date();
     }
 }
