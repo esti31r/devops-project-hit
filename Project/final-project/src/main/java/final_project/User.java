e227645faadcbaf6632fa63d887b9909138deb56
tomer idan <105118970+tomerIdan@users.noreply.github.com>
2024-08-18 19:47:29 +0300

    Update User.java

--- Project/final-project/src/main/java/final_project/User.java
+++ Project/final-project/src/main/java/final_project/User.java
@@ -6,8 +6,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import java.util.Date;
-
 /**
  * User entity class.
  * This class is mapped to a database table named app_user.
@@ -19,32 +17,21 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String jobName;
-    private String status;
-    private Date createdAt;
-    private Date updatedAt;
-    private String jobType;
-
-
+    private String username;
+    private String email;
+    private String phone;
+    private String address;
+    private String password;
 
     public User() {}
 
-    public User(String jobName, String status, Date createdAt, Date updatedAt, String jobType) {
-        this.jobName = jobName;
-        this.status = status;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.jobType = jobType;
+    public User(String username, String email, String phone, String address, String password) {
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.password = password;
     }
-
-    public User(String jobName, String status, Date createdAt, Date updatedAt) {
-        this.jobName = jobName;
-        this.status = status;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.jobType = ""; // or this.password = UUID.randomUUID().toString()); this for creat password automatically
-    }
-
 
     // Getters and Setters
     public Long getId() {
@@ -55,47 +42,55 @@ public class User {
         this.id = id;
     }
 
-    public String getJobName() {
-        return jobName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setJobName(String name) {
-        this.jobName = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getStatus() {
-        return status;
+    public String getEmail() {
+        return email;
     }
 
-    public void setStatus(String email) {
-        this.status = email;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public Date getCreatedAt() {
-        return createdAt;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setCreatedAt(Date phone) {
-        this.createdAt = phone;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
-    public Date getUpdatedAt() {
-        return updatedAt;
+    public String getAddress() {
+        return address;
     }
 
-    public void setUpdatedAt(Date address) {
-        this.updatedAt = updatedAt;
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", name='" + jobName + '\'' +
-                ", email='" + status + '\'' +
-                ", phone='" + createdAt + '\'' +
-                ", address='" + updatedAt + '\'' +
-                ", password='" + jobType + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 }
